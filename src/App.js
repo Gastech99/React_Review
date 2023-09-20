5fb4e93d1abb21f0db320baf487498c871baf0e6
Gastech99 <diabygassamad16@gmail.com>
2023-09-20 10:48:57 +0000

    Utilisation des props children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Buttons from './components/Buttons';
+import Counter from './components/Counter';
 
 function App() {
+  let number = 0
   return (
     <div className="App">
       <h1>Ceci est une révision de React JS</h1>
       <p>Compteur</p>
+      <Counter>{number}</Counter>
       <Buttons content="-"/>
       <Buttons content="+"/>
     </div>
---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Counter({children}) {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default Counter
